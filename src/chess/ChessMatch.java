7dcd9b9e240583ca6618bd567807d8386e995e03
Alexsandro Morel <alexsandrofreitasmorel@gmail.com>
2021-05-06 16:45:41 -0300

    create and place the pieces om board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch
 {
@@ -9,6 +12,7 @@ public class ChessMatch
 	public ChessMatch()
 	{
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces()
@@ -22,5 +26,11 @@ public class ChessMatch
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup()
+	{
+		board.placePiece(new Rook(board, Color.WHITE), new Position(1,2));
+		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
 	}
 }
